c5dc5fa1b03cac47bf9daf8d8377721536319d70
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2019-06-04 13:16:22 +0200

    Finish dependencies-injector

--- src/RouterManager.php
+++ src/RouterManager.php
@@ -1,7 +1,15 @@
 <?php
 namespace App;
+use DI\Container;
 class RouterManager
 {
+
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this -> container = $container;
+    }
     public function dispatch(string $requestMethod, string $requestUri, \FastRoute\Dispatcher $dispatcher )
     {
             $route = $dispatcher->dispatch($requestMethod, $requestUri);
@@ -11,12 +19,11 @@ class RouterManager
                    echo "<h1>NOT FOUND </h1>";
                    break;
                 case \FastRoute\Dispatcher::FOUND:
-                    $data = $route[1];
-                    $controller = $data[0];
-                    $method = $data[1];
-                    $objController = new $controller();
-                    $objController -> $method();
+                    $controller = $route[1];
+                    $method = $route[2];
+                    $this->container->call($controller, $method);
                     break;
+
                 case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
                     header("HTTP/1.0 405 Method not Allowed");
                     echo "<h1> METHOD NOT ALLOWED </h1>";
--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,8 +1,18 @@
 <?php
 namespace App\controllers;
 use App\ViewManager;
+use DI\Container;
+
 class HomeController
 {
+
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this -> container = $container;
+    }
+
     public function index(){
         $viewManager = new ViewManager();
         $viewManager->renderTemplate("index.view.html");
